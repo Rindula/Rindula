1f594d027776ac3c58bed547011597333e63316f
Sven Nolting <Rindula@users.noreply.github.com>
2022-01-13 16:02:10 +0100

    Adapt description fetching from XING

--- fetcher/cv/app.py
+++ fetcher/cv/app.py
@@ -2,33 +2,37 @@ import requests
 from lxml import html
 from googletrans import Translator
 from datetime import date
+import json
 # Fetch CV from Xing
 
 translator = Translator()
 resp = requests.get("https://www.xing.com/profile/Sven_Nolting3")
 
-tree = html.fromstring(resp.content)
-cvi = tree.xpath("/html/body/div[1]/div[2]/div/div[2]/section/div/main/div/div/div[2]/div/div/div[2]/div[2]/ul")
+json_data = resp.content.split("APOLLO_STATE=")[1].split(";")[0]
+resp_data = json.loads(json_data)
 
-listelements = cvi[0].getchildren()
+listelements = []
+
+i = 0
+while (True):
+    try:
+        listelements.append(resp_data['ROOT_QUERY']['$ROOT_QUERY.profileWorkExperience({"profileId":"Sven_Nolting3"}).collection.'.i])
+    except:
+        break
+    i += 1
 
 elements = []
 
 for l in listelements:
-    title = translator.translate(l.xpath(".//h4/text()")[0]).text
-    ps = l.xpath(".//p/descendant-or-self::*/text()")
-    employer = ps[-1]
-    timespan = translator.translate(''.join(ps[0:-1])).text
-    elements.append(f"- {timespan} - {title} ({employer})")
-
-with open("../../README.md", "r") as f:
-    readme = f.read()
+    title = translator.translate(l["jobTitle"]).text
+    elements.append(f"- {l['localizedTimeString']} - {title} ({l['companyName']})")
 
 today = date.today()
 born = date.fromisoformat("2000-07-13")
-
-readme = readme.replace("cv_replace_var", '\n'.join(elements))
-readme = readme.replace("age_years", str(today.year - born.year - ((today.month, today.day) < (born.month, born.day))))
+with open("../../README.md", "r") as f:
+    readme = f.read()
+    readme = readme.replace("cv_replace_var", '\n'.join(elements))
+    readme = readme.replace("age_years", str(today.year - born.year - ((today.month, today.day) < (born.month, born.day))))
 
 with open("../../README.md", "w") as f:
     f.write(readme)
